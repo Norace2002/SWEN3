4047e3c4bee91372aab99b9654b7f7bc1c2469be
Raphael <pusam.raphy@gmail.com>
2024-10-03 20:36:46 +0200

    Changes: - added routing functionality
    !!! not working
    - when not importing react-router-dom it has an issue
    - when react-router-dom is imported nginx tries static routing
    - trying to disable static routing and isntead let it handle everything via index.html it crashes the yarn build
    ...

--- webserver/src/App.js
+++ webserver/src/App.js
@@ -1,24 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
+/*
+import './paperless/landing.js';
+import './paperless/upload.js';
+import './paperless/preview.js';
+*/
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+        <Routes>
+          <!-- default path leads (for now) to standard react page -->
+          <Route exact path="/">
+            <div className="App">
+              <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                  Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                  Learn React
+                </a>
+              </header>
+            </div>
+          </Route>
+
+          <!-- documents landing page -->
+          <Route exact path="/documents">
+            /documents reached
+          </Route>
+
+          <!-- documents upload page -->
+          <Route exact path="/documents/upload">
+            /documents/upload reached
+          </Route>
+
+          <!-- document preview/info page -->
+          <Route exact path="/documents/{id}">
+            /documents/id reached
+          </Route>
+        </Routes>
+      </BrowserRouter>
   );
 }
 
