2ddf5141025f98ff6fc55f880ccb5824f2950207
Nino <if22b023@technikum-wien.at>
2024-11-17 18:25:30 +0100

    - fixed minIO connection

--- REST/out/src/main/java/paperless/services/DocumentService.java
+++ REST/out/src/main/java/paperless/services/DocumentService.java
@@ -108,10 +108,11 @@ public class DocumentService {
     //ToDo:
     // * find out if this actually works
     // * work with filepath/classpath/?
+    // * Test minIO Download
     public ResponseEntity<Resource> downloadDocumentResponse(String id){
         Optional<Document> optionalDocument = documentRepository.findById(id);
 
-        //ToDo:Test minIO Download
+
         String filecontent = new String(minIOStorage.download(id));
 
         if(optionalDocument.isPresent()) {
@@ -167,7 +168,7 @@ public class DocumentService {
             byte[] byteArray = pdfFile.getBytes();
 
             // minIO store File
-            minIOStorage.upload(documentModel.getTitle(),byteArray);
+            minIOStorage.upload(documentModel.getId(), byteArray);
 
             // save document data
             documentRepository.save(documentModel);
