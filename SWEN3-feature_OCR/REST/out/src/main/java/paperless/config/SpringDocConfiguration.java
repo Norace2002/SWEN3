0897fcc4711c670774eb619d6c544f18866a5afe
Norace2002 <114053173+Norace2002@users.noreply.github.com>
2024-11-17 19:03:53 +0100

    Add files via upload

---
+++ SWEN3-feature_OCR/REST/out/src/main/java/paperless/config/SpringDocConfiguration.java
@@ -0,0 +1,33 @@
+package paperless.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+public class SpringDocConfiguration {
+
+    @Bean(name = "paperless.config.SpringDocConfiguration.apiInfo")
+    OpenAPI apiInfo() {
+        return new OpenAPI()
+                .info(
+                        new Info()
+                                .title("Document Manager System Server")
+                                .description("Lorem ipsum dolor sit amet")
+                                .contact(
+                                        new Contact()
+                                                .email("if22b010@technikum-wien.at")
+                                )
+                                .version("1.0.11")
+                )
+        ;
+    }
+}
---
+++ SWEN3-feature_OCR/webserver/src/paperless/preview.js
@@ -0,0 +1,12 @@
+import Sidebar from "./sidebar";
+import React from "react";
+
+function previewPage(){
+    return(
+        <div>
+            preview Page
+        </div>
+    );
+}
+
+export default previewPage();
