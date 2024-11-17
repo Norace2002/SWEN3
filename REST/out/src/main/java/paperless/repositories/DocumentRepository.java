fa3c41658c80e3bcba34f73159c08b77cc8c55eb
Norace2002 <114053173+Norace2002@users.noreply.github.com>
2024-11-17 18:54:04 +0100

    Delete REST directory

--- REST/out/src/main/java/paperless/repositories/DocumentRepository.java
+++
@@ -1,9 +0,0 @@
-package paperless.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import paperless.models.Document;
-
-@Repository
-public interface DocumentRepository extends JpaRepository<Document, String> {
-}
