ff1f61af0c99678c4fd85fad5d3530c15f425071
Raphael <pusam.raphy@gmail.com>
2024-12-10 15:38:18 +0100

    Changes: - fixed jna to version 4.1.0 to work with ghost4j - fixed tesseract to version 4.1.0 to work with jna - modified OcrService.java for testing - added .dockerignore for speed during setup

--- OCR/src/main/java/OCR/service/OcrService.java
+++ OCR/src/main/java/OCR/service/OcrService.java
@@ -8,12 +8,14 @@ import org.springframework.stereotype.Service;
 
 import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.TesseractException;
+import org.springframework.util.ResourceUtils;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.RenderedImage;
 import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.util.List;
 
 @Service
@@ -26,8 +28,8 @@ public class OcrService {
 
     private String performOCR(File file) {
         Tesseract tesseract = new Tesseract();
-        tesseract.setDatapath("/app/tessdata_best");
         try {
+            tesseract.setDatapath("/app/tessdata_best");
             return tesseract.doOCR(file);
         } catch (TesseractException e) {
             return null;
@@ -35,38 +37,72 @@ public class OcrService {
     }
 
     public File convertPdfToImage(byte[] pdfBytes) throws Exception {
-        // load PDF-Document
+        // Load the PDF document
         PDFDocument document = new PDFDocument();
         document.load(new ByteArrayInputStream(pdfBytes));
+        System.out.println("ghost4j pdfdocument created from bytes");
 
-        // konfigure renderer
+        // Create a renderer and set the resolution
         SimpleRenderer renderer = new SimpleRenderer();
-        renderer.setResolution(300); // 300 DPI
+        renderer.setResolution(300); // Set desired DPI
+        System.out.println("ghost4j renderer created and resolution set");
 
-        // render
+        // Render the document as a list of images
         List<Image> images = renderer.render(document);
+        System.out.println("ghost4j list of images rendered");
 
-        // save image
+        // Convert the first image to RenderedImage
+        RenderedImage renderedImage = (RenderedImage) images.get(0);
+        System.out.println("first elmeent in list of images converted to RenderedImage");
+
+        // Save the first image to a temporary file
         File outputImage = File.createTempFile("output", ".png");
-        ImageIO.write((RenderedImage) images.get(0), "png", outputImage);
+        ImageIO.write(renderedImage, "png", outputImage);
+        System.out.println("written the renderedimage to outputimage File");
 
         return outputImage;
     }
 
     public void returnFileContent(String fileIdentifier) throws Exception {
-        // download file bytes from minio
-        byte[] byteStream = minIOService.download(fileIdentifier);
+        try{
+            // download file bytes from minio
+            byte[] byteStream = minIOService.download(fileIdentifier);
 
-        // render image from bytestream
-        File file = convertPdfToImage(byteStream);
+            if(byteStream != null){
+                System.out.println("ByteStream for id " + fileIdentifier + " retrieved from MinIO");
+            } else{
+                throw new Exception("Bytestream received is empty");
+            }
 
-        // perform OCR
-        String fileText = performOCR(file);
+            // render image from bytestream
+            File file = convertPdfToImage(byteStream);
+            System.out.println("ByteStream for id " + fileIdentifier + " converted to image with ghostScript");
 
-        if(fileText != null){
-            rabbitMqSender.returnFileContent(fileText);
-        } else{
-            System.out.println("problem with OCRService");
+            // perform OCR
+            String fileText = performOCR(file);
+            System.out.println("OCR performed on image " + fileIdentifier + " with Tesseract");
+
+            // TODO:
+            // send text to elasticSearch
+
+            // TODO:
+            // clean up after file
+            if(file.exists()){
+                boolean deleted = file.delete();
+
+                if(deleted){
+                    System.out.println("file cleanup successful");
+                } else{
+                    System.out.println("failed to delete file");
+                }
+            }
+
+            // return message that OCR was successful
+            if(fileText != null){
+                rabbitMqSender.returnFileContent("OCR extraction was successful");
+            }
+        }catch(Exception e){
+            e.printStackTrace();
         }
     }
 
